fa4af43eefb7c8e3707704a39d147b7e481bd596
Ben Jammin <blohrman@huskers.unl.edu>
2021-01-31 02:33:52 -0600

    completed tagscontroller

--- server/app/controllers/tags_controller.rb
+++ server/app/controllers/tags_controller.rb
@@ -15,15 +15,36 @@ class TagsController < ApplicationController
 
   def create
     # TODO: send back newly created connection of tag to group
+    @tags = params[:tags]
+
+    @tags.each do |tag|
+
+      id_array = []
+      tag_ids = Group.find(params[:group_id]).tags.select("tags.id")
+      tag_ids.each do |x|
+        id_array.push(x[:id])
+      end
+
+      if !(id_array.include?(tag))
+        GroupsTags.create(group_id: params[:group_id], tag_id: tag)
+      end
+    end
+
     respond_to do |format|
-      format.json { render json: { success: "new tag created test" } }
+      format.json { render json: { status: "complete" } }
     end
   end
 
   def destroy
     # TODO: remove connection of tag to group
+
+    @group = params[:group_id]
+    @tag_id = params[:id]
+
+    ActiveRecord::Base.connection.execute("DELETE FROM groups_tags WHERE group_id = #{@group} AND tag_id = #{@tag_id}")
+
     respond_to do |format|
-      format.json { render json: { tag: "tag removed from group test" } }
+      format.json { render json: { status: "success" } }
     end
   end
 end
