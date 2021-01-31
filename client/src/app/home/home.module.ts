02313168182286c49a75bc36130fbb179a12fcb0
Daniel Noon <supercookies@outlook.com>
2021-01-31 03:53:40 -0600

    start homepage

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { ComponentsModule } from "../components/components.module";
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
+    ComponentsModule,
     HomePageRoutingModule
   ],
   declarations: [HomePage]
